8e6f32a085400d911c7ef94cdaca15c6fdf74567
BraynSramos <braynramos97@gmail.com>
2025-12-29 14:10:39 -0500

    [ADD-UPDATE] 👾 Add a new responsive header component and comment out Astro site configuration. #011525

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  site: "https://brayansramos.github.io",
+  //site: "https://brayansramos.github.io",
   base: "/responsivedesign/",
 
 });
