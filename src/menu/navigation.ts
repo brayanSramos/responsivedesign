26758b99bbcc42943cba81c7375a75e18e8b7c1e
BraynSramos <braynramos97@gmail.com>
2025-12-30 07:31:06 -0500

    [ADD] 👾 navigation menu items and update Astro base path configuration. #012325

--- src/menu/navigation.ts
+++ src/menu/navigation.ts
@@ -1,6 +1,6 @@
 export const menuItems = [
-    { name: "Servicios", href: "/responsivedesign/servicios" },
-    { name: "Portafolio", href: "/responsivedesign/portafolio" },
-    { name: "Clientes", href: "/responsivedesign/clientes" },
-    { name: "Contacto", href: "/responsivedesign/contacto" },
+    { name: "Servicios", href: "/servicios" },
+    { name: "Portafolio", href: "/portafolio" },
+    { name: "Clientes", href: "/clientes" },
+    { name: "Contacto", href: "/contacto" },
 ];
