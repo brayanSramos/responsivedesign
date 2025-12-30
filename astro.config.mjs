51d9c57b63afb63745ddea387c959e9026ecdf7e
BraynSramos <braynramos97@gmail.com>
2025-12-30 07:02:59 -0500

    [UPDATE] move subdirectory path from Astro `site` to `base` configuration #012025

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,6 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  site: "https://brayansramos.github.io/responsivedesign/",
-  base: "/"
+  site: "https://brayansramos.github.io",
+  base: "/responsivedesign/"
 });
